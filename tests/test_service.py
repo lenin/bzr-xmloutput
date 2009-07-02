21cfacae2f7dae788ad57797e3585c65ffc76775
Guillermo Gonzalez <guillo.gonzo@gmail.com>
2009-07-02 13:11:58 -0300

    make the service tests work again, still leaking threads

--- tests/test_service.py
+++ tests/test_service.py
@@ -8,16 +8,12 @@ from bzrlib import tests
 from bzrlib import transport
 import os, sys
 
-host = 'localhost'
-port = 8080
 
 class XMLRPCServer(transport.Server):
     """A test server for http transports.
 
     Subclasses can provide a specific request handler.
     """
-
-    _xmlrpcd = None
 
     # used to form the url that connects to this server
     _url_protocol = 'http'
@@ -32,20 +28,24 @@ class XMLRPCServer(transport.Server):
         self.protocol_version = protocol_version
         # Allows tests to verify number of GET requests issued
         self.GET_request_nb = 0
+        self._xmlrpcd = None
+        self.host = 'localhost'
+        self.port = 0
 
-    def _get_xmlrpcd(xmlrpcServer):
-        if XMLRPCServer._xmlrpcd is None:
-            XMLRPCServer._xmlrpcd =  BzrXMLRPCServer((host, port)) 
-            register_functions(XMLRPCServer._xmlrpcd)
-        return XMLRPCServer._xmlrpcd
+    def _get_xmlrpcd(self):
+        if self._xmlrpcd is None:
+            self._xmlrpcd =  BzrXMLRPCServer((self.host, self.port))
+            register_functions(self._xmlrpcd)
+            host, self.port = self._xmlrpcd.socket.getsockname()
+        return self._xmlrpcd
 
     def _xmlrpc_start(self):
         """Server thread main entry point. """
         self._xmlrpc_running = False
         try:
             try:
-                xmlrpcd = XMLRPCServer._get_xmlrpcd(self)
-                self._xmlrpc_base_url = 'http://%s:%s/' % (host, port)
+                xmlrpcd = self._get_xmlrpcd()
+                self._xmlrpc_base_url = 'http://%s:%s/' % (self.host, self.port)
                 self._xmlrpc_running = True
             except:
                 # Whatever goes wrong, we save the exception for the main
@@ -73,7 +73,7 @@ class XMLRPCServer(transport.Server):
 
     def setUp(self, backing_transport_server=None):
         """See bzrlib.transport.Server.setUp.
-        
+
         :param backing_transport_server: The transport that requests over this
             protocol should be forwarded to. Note that this is currently not
             supported for HTTP.
@@ -111,7 +111,7 @@ class XMLRPCServer(transport.Server):
 
     def tearDown(self):
         """See bzrlib.transport.Server.tearDown."""
-        XMLRPCServer._xmlrpcd.shutdown()
+        self._xmlrpcd.shutdown()
         self._xmlrpc_running = False
         # We don't need to 'self._http_thread.join()' here since the thread is
         # a daemonic one and will be garbage collected anyway. Joining just
@@ -127,12 +127,13 @@ class TestXmlRpcServer(tests.TestCase):
             self.server = XMLRPCServer()
             self.server.setUp()
             self.addCleanup(self.server.tearDown)
-        self.client = xmlrpclib.Server("http://%s:%s" % (host, str(port)))
+        self.client = xmlrpclib.Server("http://%s:%s" % (self.server.host,
+                                                         str(self.server.port)))
 
     def test_hello(self):
         response = self.client.hello()
         self.assertEquals(response, "world!")
-        
+
     def test_run_bzr(self):
         exit, out, err = self.client.run_bzr(['bzr', 'xmlversion'], '.')
         self.assertEquals(exit, 0)
